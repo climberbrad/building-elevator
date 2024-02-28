120dc25eb202044bf26fcb203e8aacdbcee6156b
climberbrad <climberbrad@gmail.com>
2024-02-27 20:48:16 -0700

    binary search

---
+++ src/util/SearchUtil.test.ts
@@ -0,0 +1,78 @@
+import {closestTo, sortListToClosest, sortListToClosestWithTime} from "./SearchUtil.ts";
+
+test('Binary Search find closest lower', () => {
+    const currentFloor: number = 6;
+    const destinations: number[] = [1, 2, 3, 5, 8, 9, 10, 11, 12, 13, 14]
+    const closest = closestTo(currentFloor, destinations)
+    expect(closest).toEqual(5)
+})
+
+test('Binary Search find closest higher', () => {
+    const currentFloor: number = 6;
+    const destinations: number[] = [1, 2, 3, 5, 7, 9, 10, 11, 12, 13, 14]
+    const closest = closestTo(currentFloor, destinations, true)
+    expect(closest).toEqual(7)
+})
+
+test('Binary Search single destination', () => {
+    const currentFloor: number = 6;
+    const destinations: number[] = [1]
+    const closest = closestTo(currentFloor, destinations)
+    expect(closest).toEqual(1)
+})
+
+test('Binary Search two lower destinations', () => {
+    const currentFloor: number = 6;
+    const destinations: number[] = [1,2]
+    const closest = closestTo(currentFloor, destinations)
+    expect(closest).toEqual(2)
+})
+
+test('Binary Search two higher destinations', () => {
+    const currentFloor: number = 6;
+    const destinations: number[] = [11,12]
+    const closest = closestTo(currentFloor, destinations)
+    expect(closest).toEqual(11)
+})
+
+test('Binary Search tie goes to higher number', () => {
+    const currentFloor: number = 6;
+    const destinations: number[] = [5,7]
+    const closest = closestTo(currentFloor, destinations, true)
+    expect(closest).toEqual(7)
+})
+
+test('Binary Search tie goes to lower number', () => {
+    const currentFloor: number = 6;
+    const destinations: number[] = [5,7]
+    const closest = closestTo(currentFloor, destinations, false)
+    expect(closest).toEqual(5)
+})
+
+test('Binary Search default to lower number', () => {
+    const currentFloor: number = 6;
+    const destinations: number[] = [5,7]
+    const closest = closestTo(currentFloor, destinations, false)
+    expect(closest).toEqual(5)
+})
+
+test('Sort entire list by closest starting with current', () => {
+    const currentFloor: number = 12;
+    const destinations: number[] = [2,9,1,32]
+
+    const result = sortListToClosest(currentFloor, destinations)
+    expect(result).toEqual([9,2,1,32])
+})
+
+test('Return Stat for trip', () => {
+    const currentFloor: number = 12;
+    const destinations: number[] = [2,9,1,32]
+
+    const result = sortListToClosestWithTime(currentFloor, destinations, 10)
+    expect(result).toEqual(
+        {
+            currentFloor: 12,
+            destinations: [9,2,1,32],
+            totalTime: 420,
+        })
+})
---
+++ src/util/SearchUtil.ts
@@ -0,0 +1,78 @@
+// recurse down orderedList until there are two values left, then choose the closer value
+import {ElevatorStat} from "./ElevatorUtil.ts";
+
+function binarySearch(current: number, orderedList: number[], tieGoesToHigherNumber?: boolean): number[] {
+    const middleIndex: number = Math.floor(orderedList.length / 2);
+
+    if (orderedList.length === 2) {
+
+        // In a tie: default to the lower number if tieGoesToHigherNumber is not provided
+        if (Math.abs(current - orderedList[0]) === Math.abs(current - orderedList[1])) {
+            return tieGoesToHigherNumber ? [orderedList[1]] : [orderedList[0]]
+        }
+
+        // return the closest number as an array with length 1
+        return Math.abs(current - orderedList[0]) < Math.abs(current - orderedList[1])
+            ? [orderedList[0]]
+            : [orderedList[1]];
+    }
+
+    // keep splitting the array until there is only 2 items left
+    if (orderedList.length > 2) {
+        if (current < orderedList[middleIndex]) {
+            return binarySearch(current, orderedList.slice(0, middleIndex), tieGoesToHigherNumber)
+        } else if (current > orderedList[middleIndex]) {
+            return binarySearch(current, orderedList.slice(middleIndex, orderedList.length), tieGoesToHigherNumber)
+        }
+    }
+
+    return orderedList;
+}
+
+export const closestTo = (current: number, unorderedList: number[], tieGoesToHigherNumber?: boolean): number => {
+    if (unorderedList.length === 0) return current;
+    if (unorderedList.length === 1) return unorderedList[0];
+
+    // de-dup and sort destinations
+    const deDup = new Set(unorderedList)
+    const sortedList = Array.from(deDup).sort((a, b) => a - b);
+
+    // binarySearch will always return single item array
+    const result: number[] = binarySearch(current, [...sortedList], tieGoesToHigherNumber)
+
+    // return -1 if all goes wrong
+    return result.length === 1 ? result[0] : -1;
+}
+
+export const sortListToClosest = (current: number, unorderedList: number[]): number[] => {
+    const orderedByClosest: number[] = [closestTo(current, unorderedList)]
+
+    for (let i = 0; i < unorderedList.length - 1; i++) {
+        const closest = closestTo(
+            orderedByClosest[i],
+            unorderedList
+                .filter(item => !orderedByClosest.includes(item))
+        );
+        orderedByClosest.push(closest)
+    }
+    return orderedByClosest;
+}
+
+export const sortListToClosestWithTime = (current: number, unorderedList: number[], timePerFloor: number): ElevatorStat => {
+    const firstStop = closestTo(current, unorderedList);
+    const orderedByClosest: number[] = [firstStop]
+    let totalTime = Math.abs(current - firstStop) * timePerFloor;
+
+    for (let i = 0; i < unorderedList.length - 1; i++) {
+        const current = orderedByClosest[i];
+
+        const closest = closestTo(
+            current,
+            unorderedList.filter(item => !orderedByClosest.includes(item))
+        );
+
+        totalTime += Math.abs(current - closest) * timePerFloor
+        orderedByClosest.push(closest);
+    }
+    return {currentFloor: current, destinations: orderedByClosest, totalTime: totalTime};
+}
